55fdf858506f352a38763d26491022f110b068a7
Rorvik <stianlr@stud.ntnu.no>
2022-04-11 22:57:04 +0200

    started the code documentation on the cookieLogic part of the code

--- Scripts/cookieLogic.js
+++ Scripts/cookieLogic.js
@@ -7,6 +7,7 @@ import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-an
 
 //<--------------------------------------------------------- COOKIE LOGIC ---------------------------------------------------------------------------------------->
 
+/** The "checkConsentStatus" function is  */
 function checkConsentStatus() {
 
     const consentStatus = localStorage.getItem('consent');
@@ -28,9 +29,12 @@ function checkConsentStatus() {
     }
 }
 
+/** Code that fetches the consentCookieAcceptButton button in the login panel and adds the relevant function to it */
 document.getElementById("consentCookieAcceptButton").onclick = function () {
     cookieAccept();
 }
+
+/** Code that fetches the consentCookieAcceptButtonPrivacyTab button in the login panel and adds the relevant function to it */
 document.getElementById("consentCookieAcceptButtonPrivacyTab").onclick = function () {
     cookieAccept();
     document.getElementById("privacyPolicyContainer").style.display = "none";
@@ -42,9 +46,12 @@ function cookieAccept() {
     getAnalytics(firebase);
 }
 
+/** Code that fetches the consentCookieDenyButton button in the login panel and adds the relevant function to it */
 document.getElementById("consentCookieDenyButton").onclick = function () {
     cookieDeny();
 }
+
+/** Code that fetches the consentCookieDenyButtonPrivacyTab button in the login panel and adds the relevant function to it */
 document.getElementById("consentCookieDenyButtonPrivacyTab").onclick = function () {
     cookieDeny();
     document.getElementById("privacyPolicyContainer").style.display = "none";
@@ -55,6 +62,7 @@ function cookieDeny() {
     document.getElementById("cookieContainer").style.display = "none";
 }
 
+/** Code that fetches the cookiePreferenceButton button in the login panel and adds the relevant function to it */
 document.getElementById("cookiePreferenceButton").onclick = function () {
     showPrivacyPolicy();
 }
@@ -64,6 +72,7 @@ function showPrivacyPolicy() {
     document.getElementById("cookieContainer").style.display = "none";
 }
 
+/** Code that fetches the cookieSettingsButton button in the login panel and adds the relevant function to it */
 document.getElementById("cookieSettingsButton").onclick = function () {
     showPrivacyPolicy();
 }
