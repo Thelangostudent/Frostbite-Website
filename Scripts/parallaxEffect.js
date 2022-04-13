be3d66500f57c0debcb23e091e3925270283cf1a
Rorvik <stianlr@stud.ntnu.no>
2022-04-13 19:37:13 +0200

    documented the parallax effect and the scroll fade animation

--- Scripts/parallaxEffect.js
+++ Scripts/parallaxEffect.js
@@ -5,21 +5,35 @@ import {isMobile} from './onLoadFunctions.js'
 
 //<----------------------------------------------------- PARALLAX EFFECT --------------------------------------------------------------------------------------------->
 
+/** The parallax function has the main responsibility to assure a smooth and unique scrolling effect on the website by using a multi-layered design that gives
+    the visitor an illusion of depth. The parallax effect is only enabled on computers, not mobiles, due to potential performance issues on older phones.
+    This makes the website accessible and friendly to as many platforms and devices as possible. The function mainly makes the background image scroll slower than the rest
+    of the page using a mathematical offset defined in the Y-value constant */
 document.getElementById("body").onscroll = function para() {
+    //checks if the device is not a mobile
     if (isMobile === false) {
         const scrolltotop = document.scrollingElement.scrollTop;
         const target = document.getElementById("backgroundPara");
         const xvalue = "center";
         const factor = 0.5;
+
+        //gets the windowWidth and innerWidth of the users device and calculates the offset accordingly
         const windowWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
         const yvalue = (windowWidth * 0.040) + (scrolltotop * factor);
+
+        //applies the new offset to the first layer of the website (the background image)
         target.style.backgroundPosition = xvalue + " " + yvalue + "px";
     }
 }
 
+/** The fixImage function is activated everytime the user resizes the window, this is because the mathematical offset is based on the clientWidth to the users device,
+ and when the window is resized, so is the clientWidth. By activating this function whenever the window is resized, the parallax effect will recalculate its offset
+ and display the layers accordingly, this makes the parallax effect suitable for all window sizes */
 window.onresize = function fixImage() {
     if (isMobile === false) {
         const doc = document.documentElement;
+
+        //applies the new offset
         const left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
         const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
         window.scroll(left + 1, top + 1);
--- Scripts/scrollFade.js
+++ Scripts/scrollFade.js
@@ -1,29 +1,43 @@
-// ScrollFade 0.2
+//<----------------------------------------------------------- IMPORTS ------------------------------------------------------------------------------------------>
 
+//none
+
+//<-------------------------------------------------------- PARALLAX EFFECT ------------------------------------------------------------------------------------->
+
+//gets all elements marked with the scrollFade class
 const fadeElements = document.getElementsByClassName('scrollFade');
 
+/** The scrollFade function is a function that hides/shows content in the main container as the user scrolls down the page, the element is by default hidden, but when scrolled
+ into view, a short fade animation will be activated to display the item, this gives the website extra depth and quality. The main animation is defined in the CSS file as "scrollFade"
+ *  The scrollFade effect is adapted from Jordan Trbuhovics (meddlenz) open source github example found here "https://github.com/meddlenz/ScrollFade" */
 function scrollFade() {
-	for (let index = 0; index < fadeElements.length; index++) {
-		const element = fadeElements[index];
-		const rect = element.getBoundingClientRect();
+    for (let index = 0; index < fadeElements.length; index++) {
+        const element = fadeElements[index];
+        const rect = element.getBoundingClientRect();
 
-		const elementFourth = rect.height / 4;
-		const fadeInPoint = window.innerHeight - elementFourth;
-		if (rect.top <= fadeInPoint) {
-			element.classList.add('scrollFade--visible');
-			element.classList.add('scrollFade--animate');
-			element.classList.remove('scrollFade--hidden');
-		} else {
-			element.classList.remove('scrollFade--visible');
-			element.classList.add('scrollFade--hidden');
-		}
-
-
-	}
+        //defines the fade in point, when the animation is activated and the element is shown
+        const elementFourth = rect.height / 4;
+        const fadeInPoint = window.innerHeight - elementFourth;
+        if (rect.top <= fadeInPoint) {
+            //shows elements in the main container via a fade animation when the user is scrolling down
+            element.classList.add('scrollFade--visible');
+            element.classList.add('scrollFade--animate');
+            element.classList.remove('scrollFade--hidden');
+        } else {
+            //hides elements below the visible content if the user scrolls back up
+            element.classList.remove('scrollFade--visible');
+            element.classList.add('scrollFade--hidden');
+        }
+    }
 }
 
+//adds eventListeners to the scrollFade effect
 document.addEventListener('scroll', scrollFade);
 window.addEventListener('resize', scrollFade);
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     scrollFade();
 });
+
+//<----------------------------------------------------------- EXPORTS ------------------------------------------------------------------------------------------>
+
+//none
